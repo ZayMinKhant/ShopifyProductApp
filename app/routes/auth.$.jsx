df1c4519b31729dbce31c8606ab1d28c7da4f8e3
Zay Min Khant <zmk@mit.com.mm>
2025-07-25 07:55:44 +0700

    Update redirect URLs in shopify.app.toml for improved authentication flow; refactor authentication logic in auth.$.jsx to return response directly if authentication fails.

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,7 +1,7 @@
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
-
+  const response = await authenticate.admin(request);
+  if (response instanceof Response) return response;
   return null;
 };
